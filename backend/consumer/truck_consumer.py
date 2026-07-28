2c33ad24ce10a3ba0945de7915402a3c3791f208
Arjunrana23 <arjunrana0611005@gmail.com>
2026-07-28 23:01:15 +0530

    Added Kafka consumer worker logic

--- backend/consumer/truck_consumer.py
+++ backend/consumer/truck_consumer.py
@@ -10,6 +10,15 @@ conf = {
 consumer = Consumer(conf)
 
 consumer.subscribe(['truck-events'])
+# Worker state
+total_events = 0
+total_temperature = 0
+
+highest_temperature = float("-inf")
+lowest_temperature = float("inf")
+
+alert_count = 0
+
 
 print("Listening for truck events... Press Ctrl+C to stop.")
 
@@ -26,6 +35,27 @@ try:
 
         event = json.loads(msg.value().decode("utf-8"))
 
+        temperature = event["temperature"]
+
+        total_events += 1
+        total_temperature += temperature
+
+        if temperature > highest_temperature:
+            highest_temperature = temperature
+
+        if temperature < lowest_temperature:
+            lowest_temperature = temperature
+
+        average_temperature = total_temperature / total_events
+
+        if temperature > 35:
+            alert_count += 1
+
+            print("\n🚨 HIGH TEMPERATURE ALERT!")
+            print(f"Truck ID    : {event['truck_id']}")
+            print(f"Temperature : {temperature} °C")
+        
+
         print("\nReceived Event")
         print(f"Truck ID     : {event['truck_id']}")
         print(f"Temperature  : {event['temperature']} °C")
@@ -35,8 +65,18 @@ try:
         print(f"GPS          : {event['gps_location']}")
         print(f"Timestamp    : {event['timestamp']}")
 
+
+        print("\n----- Worker Statistics -----")
+        print(f"Total Events        : {total_events}")
+        print(f"Average Temperature : {average_temperature:.2f} °C")
+        print(f"Highest Temperature : {highest_temperature} °C")
+        print(f"Lowest Temperature  : {lowest_temperature} °C")
+        print(f"High Temp Alerts    : {alert_count}")
+        print("-----------------------------")
+
 except KeyboardInterrupt:
     print("\nConsumer stopped.")
 
 finally:
     consumer.close()
+    
